e84685333e0b9349f73c1f43dea59af3f07a13d9
seongtae <seongtae0205@postech.ac.kr>
2018-12-10 21:46:42 +0900

    little change in process.c

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -518,7 +518,7 @@ setup_stack (void **esp)
   uint8_t *kpage;
   bool success = false;
 
-  kpage = palloc_get_page (PAL_USER | PAL_ZERO);
+  kpage = fte_alloc(PAL_USER | PAL_ZERO)->frame;
   if (kpage != NULL) 
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
